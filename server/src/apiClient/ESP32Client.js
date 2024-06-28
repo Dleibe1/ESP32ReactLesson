414f950162937dabc7c53104d4e641ea5563dd5a
dleibe1 <daniel.leibensperger28@gmail.com>
2024-06-28 17:06:23 -0400

    first commit

--- server/src/apiClient/ESP32Client.js
+++ server/src/apiClient/ESP32Client.js
@@ -1,7 +1,7 @@
 
 import got from "got"
 
-const esp32Test = async () => {
+const esp32Test = async (messageFromFrontEnd) => {
   try {
       const response = await got('http://192.168.1.134:8080/led_on');
       return response.body
